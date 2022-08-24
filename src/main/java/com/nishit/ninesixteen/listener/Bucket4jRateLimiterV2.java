15e3d4bfac603175fce1305082aa31a50c89f21c
Nishit Patira <npatira@amazon.com>
2022-08-24 12:27:35 -0700

    New RateLimiter

--- src/main/java/com/nishit/ninesixteen/listener/Bucket4jRateLimiterV2.java
+++ src/main/java/com/nishit/ninesixteen/listener/Bucket4jRateLimiterV2.java
@@ -4,6 +4,7 @@ import static com.nishit.ninesixteen.constants.Constants.TOKEN_COUNT;
 
 import java.time.Duration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.github.bucket4j.Bandwidth;
@@ -12,21 +13,21 @@ import io.github.bucket4j.Bucket4j;
 import io.github.bucket4j.Refill;
 
 @Component
-public class Bucket4jRateLimiter {
+public class Bucket4jRateLimiterV2 {
 
-	// @Value("${service.ratelimit:100}")
-	private static int throttleRate = 5;
+	@Value("${service.ratelimit:100}")
+	private int throttleRate=5;
 
-	// @Value("${service.thrrotleDuration:1")
-	private static int refillDuration = 1;
+	@Value("${service.thrrotleDuration:1}")
+	private int refillDuration=1;
 
-	private static final Refill refill = Refill.intervally(throttleRate, Duration.ofMinutes(refillDuration));
+	private final Refill refill = Refill.intervally(throttleRate, Duration.ofMinutes(refillDuration));
 
-	private static final Bandwidth limit = Bandwidth.classic(5, refill);
+	private final Bandwidth limit = Bandwidth.classic(5, refill);
 
-	private static Bucket BUCKET = Bucket4j.builder().addLimit(limit).build();
+	private Bucket BUCKET = Bucket4j.builder().addLimit(limit).build();
 
-	public static boolean isAPICallAllowed() {
+	public boolean isAPICallAllowed() {
 		System.out.println("Available tokens are  : " + BUCKET.getAvailableTokens() + " called from thread : "
 				+ Thread.currentThread().getName());
 		return BUCKET.tryConsume(TOKEN_COUNT);
